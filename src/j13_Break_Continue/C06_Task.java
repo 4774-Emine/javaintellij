d18bce57a7b6b10b66befb118e962629562e79fc
Emine Dişçeken <haticehifa.2018@gmail.com>
2022-10-10 22:32:27 +0300

    OCA

--- src/j13_Break_Continue/C06_Task.java
+++ src/j13_Break_Continue/C06_Task.java
@@ -1,12 +1,24 @@
 package j13_Break_Continue;
 
+import java.util.Scanner;
+
 public class C06_Task {
     public static void main(String[] args) {
 
  //task-> girilen bir ifadedeki C karakterine kadar a karakter sayısını print eden code create ediniz
+Scanner sc = new Scanner(System.in);
+        System.out.println("bir string ifade giriniz");
 
-
-
-
+        String biseyler=sc.nextLine();
+        int aSayısı=0;
+        for(int i=0; i<biseyler.length(); i++) {
+            if(biseyler.charAt(i)=='a'){
+                aSayısı++;
+            }else if(biseyler.charAt(i)=='c'){
+                break;
+            }
+            System.out.println("döngüğde işleme giren karakterler:" + biseyler.charAt(i));
+        }
+        System.out.println("a sayısı= "+ aSayısı + "tane");
     }
 }
--- src/j13_Break_Continue/C5_OCA_type.java
+++ src/j13_Break_Continue/C5_OCA_type.java
@@ -4,8 +4,9 @@ public class C5_OCA_type {
     public static void main(String[] args) {
 
   int count=0;
-  ROW_LOOP:for(int row=1; row<=3;row++)
-      for(int col=1; col<=2;col++){
+  ROW_LOOP:for(int row=1; row<=3;row++)//dış loop
+      //System.out.println();iç dış loop arası komut girerse looplar arası ilişki kopar
+      for(int col=1; col<=2;col++){//iç loop
           if(row*col%2==0) continue ROW_LOOP;//row*cal çift ise bekleme yapma devam et count artma
           count++;
       }
