a285610a092474cd218922f272391d752f9f5fa5
Emine Dişçeken <haticehifa.2018@gmail.com>
2022-10-10 18:24:41 +0300

    code challenge

---
+++ src/day06/Q02_ForLoop.java
@@ -0,0 +1,27 @@
+package day06;
+
+public class Q02_ForLoop {
+    public static void main(String[] args) {
+
+        //interview question
+        //0-255'e kadar olan harflerin sayı ve harf değerini ekrana yazdırınız...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/j12_Lopps/d1_denemeClass.java
+++ src/j12_Lopps/d1_denemeClass.java
@@ -4,17 +4,78 @@ import java.util.Scanner;
 
 public class d1_denemeClass {
     public static void main(String[] args) {
-//ilk 10 doğal sayının topolamını hesaplamak için bir code create ediniz
+/*"haydi javaya koşalım" 15 kere print ediniz
+        for(int i=1; i<=15; i++){
+            System.out.println(i+".haydi javaya koşalım");
+        }
+*/
 
 
-       int toplam=0;
+/*1 den 100 e kadar olan sayıları ekrana yazdır
+        for(int i=0; i<=100;i++) {
+            System.out.print(i+"*");
+        }*/
 
-        for(int i=1;i<=10;i++){
-            toplam+=i;
-            System.out.println(toplam);
-}
+       /*1 den 5 e kadar olan sayıların karelerini ekrana yazdır
+       for (int i=1; i<=5; i++) {
+           System.out.println("karekök içine " + i * i + "=" + i + "demektir");
+       }
+*/ /*System.out.println("<-------------->");
+        for (int i = 1; i <= 8; i++) {
 
-
+            System.out.println("*\t*");
+            System.out.println("    *");
 
         }
+
+        System.out.println("<--------------->");
+    }*/
+
+    /*for     döngüsü kullanılarak 50 derece ile 60 derece arasını fahrenheit cinsinde print eden code cretae ediniz
+        for(int i=50;i<=60;i++){
+            double f=i*1.18+32;
+            System.out.println(i+ "derece"+f+"fahrenheit");}
+
+*//*
+Scanner sc=new Scanner(System.in);
+        System.out.println("lütfen iki sayı giriniz");
+int sayı1=sc.nextInt();
+int sayı2=sc.nextInt();
+int toplam=sayı1+sayı2;
+        System.out.println("toplam değeriniz: " + toplam);*/
+/*
+float sayı1=8.5f;
+float sayı2=1.8f;
+        System.out.println("çarpım="+ sayı1*sayı2+"f");*/
+
+
+        /*kullanıcının girdiği sayının negatif mi pozitif mi olduğunu gösteren bir code create ediniz
+        Scanner sc=new Scanner (System.in);
+        System.out.println("lütfen bir sayı giriniz");
+int sayı=sc.nextInt();
+if(sayı<0) {
+    System.out.println("Girdiğiniz sayı negatiftir");
+}else if(sayı>0) {
+    System.out.println("girdiğiniz sayı pozitiftir");
+}else {
+    System.out.println("Girdiğiniz sayı sıfırdır");
+}
+*/
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
     }
+}
