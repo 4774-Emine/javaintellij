2738dd1d6aa6216ed8cb1eaab57cea1734120475
Emine Dişçeken <haticehifa.2018@gmail.com>
2022-10-10 22:12:52 +0300

    devam

--- src/j13_Break_Continue/C04_PinControl.java
+++ src/j13_Break_Continue/C04_PinControl.java
@@ -1,19 +1,35 @@
 package j13_Break_Continue;
 
+import java.util.Scanner;
+
 public class C04_PinControl {
     public static void main(String[] args) {
 
  //Task-> String data type bir pin datasının 3 giriş hakkında kontrol eden code create ediniz...
 
+Scanner sc = new Scanner(System.in);
+        String dogruPin="javaCAN";
+
+        String pin=sc.nextLine();
+        int girisHakkı=3;
+        while(true) {
+            System.out.println("pin giriniz");
+            String agaPin = sc.nextLine();
+            if (agaPin.equals(dogruPin)) {
+                System.out.println("gayet başarılı");
+                break;
+            } else {
+                System.out.println("tekrar deneyiniz");
+girisHakkı--;
+                System.out.println("kalan canınız: "+ girisHakkı);
+if(girisHakkı==0){
+    System.out.println("can kalmadı");
+    break;
+}
+            }
 
 
-
-
-
-
-
-
-
+        }
 
     }
 }
---
+++ src/j13_Break_Continue/C06_Task.java
@@ -0,0 +1,12 @@
+package j13_Break_Continue;
+
+public class C06_Task {
+    public static void main(String[] args) {
+
+ //task-> girilen bir ifadedeki C karakterine kadar a karakter sayısını print eden code create ediniz
+
+
+
+
+    }
+}
