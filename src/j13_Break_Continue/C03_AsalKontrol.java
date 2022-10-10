d57f2d97e94b7c0c6c1ee7f491a034dfb9d1df22
Emine Dişçeken <haticehifa.2018@gmail.com>
2022-10-10 21:47:29 +0300

    break ve continue

---
+++ src/j13_Break_Continue/C03_AsalKontrol.java
@@ -0,0 +1,30 @@
+package j13_Break_Continue;
+
+import java.util.Scanner;
+
+public class C03_AsalKontrol {
+    public static void main(String[] args) {
+        //girilen tam sayının asal olmasını kontrol eden code create ediniz
+        //asal tamsayı: 1 ve kendisinden başka tam böleni olmayan sayılar->asil bölünemeyen sayı
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("bir tamsayı giriniz");
+        int sayi = sc.nextInt();
+
+        boolean asalMı = true;
+        for (int i = 2; i < sayi; i++) {
+            if (sayi % i == 0) {//eğer sayı kendisine kadar olan sayılara tam bölünüyorsa
+                asalMı = false;
+
+            }
+        }
+        if (asalMı) {
+            System.out.println("girilen sayı asal");
+
+        } else System.out.println("girilen sayı asal değil");
+
+
+    }
+
+}
---
+++ src/j13_Break_Continue/C04_PinControl.java
@@ -0,0 +1,19 @@
+package j13_Break_Continue;
+
+public class C04_PinControl {
+    public static void main(String[] args) {
+
+ //Task-> String data type bir pin datasının 3 giriş hakkında kontrol eden code create ediniz...
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
