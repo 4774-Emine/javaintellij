2a3f16c5a58a6a4ea2bdd14fb529d3cc559cf673
Emine Dişçeken <haticehifa.2018@gmail.com>
2022-10-11 20:30:23 +0300

    day 7 son

--- src/day07/Q06_RecursiveForLoop.java
+++ src/day07/Q06_RecursiveForLoop.java
@@ -1,7 +1,53 @@
 package day07;
 
+import java.util.Scanner;
+
 public class Q06_RecursiveForLoop {
     public static void main(String[] args) {
+
+/*  Problem Tanımı
+  Verilen iki sayının kombinasyonunu bulan kodu yazınız.
+  Hatırlatma C(n,r) = n! / (r!(n-r)!)
+
+  Verilen iki sayının permütasyonunu bulan kodu yazınız.
+  Hatırlatma P(n,r) = n! / (n-r)!
+
+  Ekran Çıktısı
+  Birinci sayıyı giriniz: 15
+  Ikinci sayıyı giriniz: 4
+  Kombinasyon: 1365
+  permütasyon: 32760
+
+  Birinci sayıyı giriniz: 5
+  Ikinci sayıyı giriniz: 3
+  Kombinasyon: 10
+  permütasyon: 60
+*/
+
+Scanner sc=new Scanner(System.in);
+        System.out.println("n değeri: ");
+        long n=sc.nextLong();
+
+        System.out.println("r değeri: ");
+        long r=sc.nextLong();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
